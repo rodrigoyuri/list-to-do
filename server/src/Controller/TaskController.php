c74498e78c22e2bd3151eb5d7d5a2bf54eec0678
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-14 19:03:46 -0300

    Prefix para route task e primeira routa setada.

--- server/src/Controller/TaskController.php
+++ server/src/Controller/TaskController.php
@@ -6,10 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/task", name="task_")
+ */
 class TaskController extends AbstractController
 {
     /**
-     * @Route("/task", name="task")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
