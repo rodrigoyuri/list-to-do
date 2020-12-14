96b2ab93b4be700963618875877d984138d418cb
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-14 19:36:37 -0300

    Metodos e verbos definidos em TaskController.

--- server/src/Controller/TaskController.php
+++ server/src/Controller/TaskController.php
@@ -3,16 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/task", name="task_")
+ * @Route("/tasks", name="task_")
  */
 class TaskController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="index", methods={"GET"})
      */
     public function index(): Response
     {
@@ -20,5 +21,37 @@ class TaskController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/TaskController.php',
         ]);
+    }
+
+    /**
+     * @Route("/{id}", name="show", methods={"GET"})
+     */
+    public function show($id)
+    {
+
+    }
+
+    /**
+     * @Route("/", name="create", methods={"POST"})
+     */
+    public function create(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/{id}", name="update", methods={"PUT", "PATCH"})
+     */
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    /**
+     * @Route("/{id}", name="delete", methods={"DELETE"})
+     */
+    public function delete($id)
+    {
+
     }
 }
