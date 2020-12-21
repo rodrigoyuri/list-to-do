3a1434ef888c238037cbff19bf8562945cc7d9fd
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-21 15:23:43 -0300

    Form de cadastrado importado em main.js e criado em FormTask.vue

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <nav-bar></nav-bar>
     <card-task></card-task>
+    <form-task></form-task>
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar';
-import CardTask from './components/CardTask'
+import CardTask from './components/CardTask';
+import FormTask from './components/FormTask';
 
 export default {
   name: 'App',
   components: {
     NavBar,
-    CardTask
+    CardTask,
+    FormTask
   }
 }
 </script>
--- web/src/components/FormTask.vue
+++ web/src/components/FormTask.vue
@@ -1,6 +1,36 @@
 <template>
     <div>
-        <h1>Hello World!!!</h1>
+        <div class="row mt-3 d-flex justify-content-center">
+            
+            <div class="col-4 border rounded">
+                <h3>Cadastre suas Tarefa</h3>
+                <div class="mb-3">
+                    <label 
+                        for="name" 
+                        class="form-label">Nome da Tarefa</label>
+                    <input 
+                        type="text" 
+                        class="form-control i" 
+                        id="name">
+                </div>
+
+                <div class="mb-3">
+                    <label 
+                        for="description" 
+                        class="form-label">Descrição</label>
+                    <input 
+                        type="text" 
+                        class="form-control i" 
+                        id="description">
+                </div>
+
+                <div class="d-flex justify-content-between mb-3">
+                    <button class="btn btn-success">Confirmar</button>
+                    <button class="btn btn-danger">Cancelar</button>
+                </div>
+
+            </div>
+        </div>
     </div>
 </template>
 
