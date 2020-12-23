1a25925366339b897d28ee500faae010c153aa4d
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-23 19:26:40 -0300

    Adição do buttonAddTask

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="home">
+    <div class="d-flex justify-content-end mr-3 mt-3">
+      <router-link to="/register-task">
+        <button 
+            class="btn btn-outline-success btn-lg"><font-awesome-icon icon="plus" />
+        </button>
+      </router-link>
+      </div>
     <card-task
       title="Tarefas Restantes"
       icon="check"
