0b1255be498bf9df4030bfae66ea911e442be9ed
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-23 15:21:04 -0300

    Pasta views criada contendo as views para projeto

---
+++ web/src/views/CompleteTask.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="container">
+        <h1>Tarefas concluidas</h1>
+    </div>
+</template>
---
+++ web/src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <CardTask v-if="isVisible"/>
+    <FormTask v-if="!isVisible"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CardTask from '@/components/CardTask'
+import FormTask from '@/components/FormTask';
+import EventBus from '@/event-bus';
+
+export default {
+  name: 'Home',
+  components: {
+    CardTask,
+    FormTask
+  },
+
+  data() {
+    return {
+      isVisible: true
+    }
+  },
+
+  created: function() {
+    const thisVue = this;
+    
+    EventBus.$on('showForm', function() {
+      thisVue.isVisible = false;
+    });
+
+    EventBus.$on('cancelRegister', function() {
+      thisVue.isVisible = true;
+    })
+
+    EventBus.$on('editTask', function() {
+      thisVue.isVisible = false;
+    })
+  }
+}
+</script>
