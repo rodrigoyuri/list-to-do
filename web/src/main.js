682fcaacd5b02768bb71a708d9386615c7d7042e
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-23 17:37:35 -0300

    Adição de novos icons, importação do Jquery.

--- web/src/main.js
+++ web/src/main.js
@@ -2,21 +2,34 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPlus, faEdit, faTrash, faCheck } from '@fortawesome/free-solid-svg-icons';
+import {
+  faPlus,
+  faEdit, 
+  faTrash, 
+  faCheck, 
+  faTasks,
+  faList,
+  faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import App from './App.vue'
 
+import jquery from 'jquery';
+global.jquery = jquery;
+global.$ = jquery;
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import router from './router'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import router from './router';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 Vue.use(BootstrapVue);
 
-library.add(faPlus, faEdit, faTrash, faCheck);
+library.add(faPlus, faEdit, faTrash, faCheck, faTasks, faList, faUserCircle);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 new Vue({
