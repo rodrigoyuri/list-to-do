d8a943aca03d776e51209483780415b1708b7e18
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-23 18:56:58 -0300

    uso do componente CardTask com suas props.

--- web/src/views/CompleteTask.vue
+++ web/src/views/CompleteTask.vue
@@ -1,5 +1,23 @@
 <template>
-    <div class="container">
-        <h1>Tarefas concluidas</h1>
+    <div class="complete">
+        <card-task 
+            title="Tarefas Concluidas" 
+            :headerCard="{
+                headerText: 'Removar tarefa',
+                styleButton: 'danger'
+            }"
+            :showButtos=false
+            icon="times"></card-task>
     </div>
 </template>
+
+<script>
+import CardTask from '../components/CardTask.vue'
+
+export default {
+    name: 'Complete',
+    components: {
+        CardTask
+    }    
+}
+</script>
