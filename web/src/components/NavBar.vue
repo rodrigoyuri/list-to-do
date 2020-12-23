489a7271d47d7c789f1b7b7eebd0e8973191e5d6
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-23 19:29:50 -0300

    Remoção do buttonAddTask, data, methods, create e style

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -25,56 +25,11 @@
 
             </div>
         </nav>
-        <div class="d-flex justify-content-end mr-3 mt-3">
-            <button 
-                class="btn btn-outline-success btn-lg"
-                v-if="showButtonAdd"
-                @click.prevent="showForm()"><font-awesome-icon icon="plus" /></button>
-        </div>
     </div>
 </template>
 
 <script>
 
-import EventBus from '../event-bus';
-
 export default {
-    data() {
-        return {
-            titles: [
-                {
-                    name: 'Symfony', 
-                    uriLogo: 'https://symfony.com/logos/symfony_black_03.png'
-                },
-                {
-                    name: 'Vuejs', 
-                    uriLogo: 'https://vuejs.org/images/logo.png'
-                },
-            ],
-            showButtonAdd: true
-        }
-    },
-
-    created: function() {
-        const thisVue = this;
-
-        EventBus.$on('cancelRegister', function() {
-            thisVue.showButtonAdd = true;
-        })
-    },
-
-    methods: {
-        showForm() {
-            this.showButtonAdd = false;
-            EventBus.$emit('showForm');
-        }
-    }
 }
 </script>
-
-<style>
-    img {
-        height: 35px;
-        width: 35px;
-    }
-</style>
