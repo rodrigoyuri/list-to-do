baece8d0ba78494b6ae126951b51ba4fc59d1125
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-23 19:31:34 -0300

    criação da view e importação e uso do component FormTask

---
+++ web/src/views/CreateTask.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="create">
+        <form-task></form-task>
+    </div>
+</template>
+
+<script>
+import FormTask from '../components/FormTask.vue'
+
+export default {
+    name: 'create',
+    components: {
+        FormTask 
+    }    
+}
+</script>
