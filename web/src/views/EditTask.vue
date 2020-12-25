cc9c8dc55aacd0d40004ce8dfce4e2c2ff1c6d73
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-25 13:57:01 -0300

    Criação da view EditTask

---
+++ web/src/views/EditTask.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="edit">
+        <form-task title="Editar Tarefa"></form-task>
+    </div>
+</template>
+
+<script>
+import FormTask from '../components/FormTask.vue';
+
+export default {
+    name: 'Edit',
+    components: {
+        FormTask
+    },
+}
+</script>
