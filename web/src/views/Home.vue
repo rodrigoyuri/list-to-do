9429fad5ac72a770822e6a86118b49bbf742935a
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-29 15:57:36 -0300

    Remoção EventBus e data que não precisam mais, além dos imports e do component FormTask

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,60 +1,33 @@
 <template>
   <div class="home">
+    
     <div class="d-flex justify-content-end mr-3 mt-3">
       <router-link to="/register-task">
         <button 
             class="btn btn-outline-success btn-lg"><font-awesome-icon icon="plus" />
         </button>
       </router-link>
-      </div>
+    </div>
+
     <card-task
       title="Tarefas Restantes"
       :headerCard="{
         headerText: ['Marcar com concluida'],
         showRemove: false
       }"
-      v-if="isVisible"
     />
-    <form-task v-if="!isVisible" />
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import CardTask from '@/components/CardTask'
-import FormTask from '@/components/FormTask';
-import EventBus from '@/event-bus';
-
-
 
 export default {
   name: 'Home',
   components: {
     CardTask,
-    FormTask
   },
-
-  data()
-    {
-    return {
-      isVisible: true
-    }
-  },
-
-  created: function() {
-    const thisVue = this;
-    
-    EventBus.$on('showForm', function() {
-      thisVue.isVisible = false;
-    });
-
-    EventBus.$on('cancelRegister', function() {
-      thisVue.isVisible = true;
-    })
-
-    EventBus.$on('editTask', function() {
-      thisVue.isVisible = false;
-    })
-  }
 }
 </script>
