2a43a2bc61a005aaf31fd2da5adc721183040b43
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-30 13:24:23 -0300

    Estrutura do footer criado.

--- web/src/components/Footer.vue
+++ web/src/components/Footer.vue
@@ -1,3 +1,31 @@
 <template>
-    <h1>Footer</h1>
+    <footer class="footer">
+        <nav class="navbar navbar-light bg-light d-flex justify-content-between">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="https://symfony.com/logos/symfony_black_03.png" alt="" width="50" height="50" class="d-inline-block align-top">
+                    Symfony
+                </a>
+
+                <a class="navbar-brand" href="#">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/120px-Vue.js_Logo_2.svg.png" alt="" width="30" height="24" class="d-inline-block align-top">
+                    VueJs
+                </a>
+            </div>
+        </nav>
+    </footer>
 </template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    .footer {
+        width: 100%;
+        position: fixed;
+        bottom: 0;
+    }
+</style>
