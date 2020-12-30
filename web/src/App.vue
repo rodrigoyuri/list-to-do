ae05b996a7f54118e798ad3ae7d79ff9d68c2df7
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-30 13:21:09 -0300

    Importação e uso do component Footer.vue

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
+    
     <nav-bar></nav-bar>
+
     <router-view/>
+
+    <Footer />
+
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
 
 export default {
     name: 'App',
     components: {
-      NavBar
+      NavBar,
+      Footer
     } 
 }
 </script>
